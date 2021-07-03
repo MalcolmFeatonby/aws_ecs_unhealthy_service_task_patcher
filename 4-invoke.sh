268bbfb536f4edd7df1a7588aae2f0d6797d5295
Malcolm Featonby <featonby@amazon.com>
2021-07-02 20:57:01 -0700

    Address gap in invoke script.

--- 4-invoke.sh
+++ 4-invoke.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-FUNCTION=$(aws cloudformation describe-stack-resource --stack-name blank-python --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
+FUNCTION=$(aws cloudformation describe-stack-resource --stack-name aws-ecs-sample-task-unsticker --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
 
 aws lambda invoke --function-name $FUNCTION out.json
 cat out.json
